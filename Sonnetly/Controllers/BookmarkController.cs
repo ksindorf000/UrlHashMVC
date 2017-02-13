0cec8588e09e57ed47a5049100aea8a39e55473e
ksindorf000 <ksindorf000@gmail.com>
2017-02-13 16:19:15 -0500

    Added Encryption Class

---
+++ Sonnetly/Controllers/BookmarkController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNet.Identity;
+using Sonnetly.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sonnetly.Controllers
+{
+    public class BookmarkController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        
+        // GET: Bookmark
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // CREATE: Bookmark
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // CREATE/POST: Bookmark
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,Desc,OriginalUrl,NewUrl,NumClicks,OwnerId,Created")] Bookmarks newBM)
+        {
+            if (ModelState.IsValid)
+            {
+                newBM.Created = DateTime.Now;
+                newBM.OwnerId = User.Identity.GetUserId();
+                //newBm.NewUrl = ConvertUrl();
+                db.Bookmarks.Add(newBM);
+                db.SaveChanges();
+
+                RedirectToAction("Detail", new { id = newBM.Id });
+            }
+
+            return View();
+        }
+
+
+        
+
+    }
+}
---
+++ Sonnetly/Helpers/EncryptURL.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Sonnetly.Helpers
+{
+    public class EncryptURL
+    {
+        private static int indexStart = 0;
+        private static int lineNum = 0;
+        public static string results;
+
+        static void Main(string[] args)
+        {
+            //Get hashed key from user input
+            var myInput = Console.ReadLine();
+            string hashKey = Encrypt256(myInput);
+
+            //Get lines
+            List<string> lineList = new List<string>();
+            for (int i = 0; i < 4; i++)
+            {
+                lineNum = GetLineNum(hashKey);
+                lineList.Add(GetLine(lineNum));
+            }
+
+            //Get random words
+            List<string> rWords = new List<string>();
+            foreach (var line in lineList)
+            {
+                rWords.Add(GetRandWord(line));
+            }
+
+            results = Results(rWords);
+        }
+
+        /*****************************************
+         * Encrypt256()
+         * Hashing code written by Joel in class
+         *****************************************/
+        static string Encrypt256(string input)
+        {
+            string output;
+            byte[] byteData = Encoding.ASCII.GetBytes(input);
+            Stream inputStream = new MemoryStream(byteData);
+
+            using (SHA256 shaM = new SHA256Managed())
+            {
+                var result = shaM.ComputeHash(inputStream);
+                output = BitConverter.ToString(result);
+            }
+
+            //Strip special characters
+            output = output.Replace("-", "");
+            //Strip letters
+            output = Regex.Replace(output, "[A-Za-z ]", "");
+
+            return output;
+        }
+
+        /*****************************************
+         * GetLineNum()
+         * Gets line num based on digits in hash
+         *****************************************/
+        private static int GetLineNum(string hashKey)
+        {
+            int lineNum = 0;
+            bool invalid = true;
+
+            //If first three numbers are out of bounds,
+            //get next three numbers
+            while (invalid)
+            {
+                lineNum = int.Parse(hashKey.Substring(indexStart, 3));
+                invalid = lineNum > File.ReadLines(@"..\..\TheSonnets.txt").Count() ? true : false;
+                indexStart++;
+            }
+
+            return lineNum;
+        }
+
+
+        /*****************************************
+         * GetLine()
+         * Gets line based on hash 
+         * Orders words in line, Desc
+         *****************************************/
+        private static string GetLine(int lineNum)
+        {
+            //Get Line from File
+            string line = File.ReadLines(@"..\..\TheSonnets.txt").Skip(lineNum).Take(1).First();
+
+            return line;
+        }
+
+        /*****************************************
+         * GetRandWord()
+         * Gets a random word from the given line
+         *****************************************/
+        private static string GetRandWord(string line)
+        {
+            string word;
+            Random rng = new Random();
+
+            List<string> sList = new List<string>(
+                line.Split(new string[] { " " }, StringSplitOptions.None));
+
+            int index = new Random().Next(sList.Count());
+            word = sList[index];
+
+            return word;
+        }
+
+
+        /*****************************************
+         * Results()
+         *****************************************/
+        private static string Results(List<string> rWords)
+        {
+            string results = string.Join(" ", rWords.ToArray());
+
+            //http://stackoverflow.com/questions/1943273/convert-all-first-letter-to-upper-case-rest-lower-for-each-word
+            results = System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(results.ToLower());
+
+            results = results.Replace(" ", "");
+
+            return results;
+        }
+    }
+}
