d952a70a052b64a1e85757531b3c3abfd5839a58
ksindorf000 <ksindorf000@gmail.com>
2017-02-13 14:04:11 -0500

    Add project files.

---
+++ Sonnetly/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sonnetly.Startup))]
+namespace Sonnetly
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
