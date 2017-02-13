bf8e16771edfd4226242ceaad73d77f59b7eb620
ksindorf000 <ksindorf000@gmail.com>
2017-02-13 15:34:00 -0500

    Initial

---
+++ Sonnetly/Models/Bookmarks.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Sonnetly.Models
+{
+    public class Bookmarks
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Desc { get; set; }
+
+        [Required]
+        [Display(Name = "Original URL")]
+        public string OriginalUrl { get; set; }
+
+        [Display(Name = "Sonnetly URL")]
+        public string NewUrl { get; set; }
+
+        [Display(Name = "Overall Clicks")]
+        public int NumClicks { get; set; }
+
+        [Display(Name = "Created by")]
+        public string OwnerId { get; set; }
+
+        [Display(Name = "Date Created")]
+        [DisplayFormat(DataFormatString = "dd MMM yyyy")]
+        public DateTime Created { get; set; }
+
+        public virtual ApplicationUser Owner { get; set; }
+
+    }
+}
--- Sonnetly/Models/IdentityModels.cs
+++ Sonnetly/Models/IdentityModels.cs
@@ -20,6 +20,8 @@ namespace Sonnetly.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Bookmarks> Bookmarks { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
