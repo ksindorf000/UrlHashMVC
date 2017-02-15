a67e9e8b24c6c03d47d20331364f8e3ef14f69d3
ksindorf000 <ksindorf000@gmail.com>
2017-02-14 21:59:22 -0500

    Added Edit and Delete

--- Sonnetly/Controllers/BookmarkController.cs
+++ Sonnetly/Controllers/BookmarkController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -21,7 +22,9 @@ namespace Sonnetly.Controllers
                 .ToList();
 
             return View();
-        }        
+        }
+
+
 
         // GET: Sonnet Details
         [Authorize]
@@ -29,9 +32,19 @@ namespace Sonnetly.Controllers
         {
             var userId = User.Identity.GetUserId();
 
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
             var sonnet = db.Bookmarks
                 .Where(b => b.Id == id && b.OwnerId == userId)
                 .FirstOrDefault();
+
+            if (sonnet == null)
+            {
+                return HttpNotFound();
+            }
 
             ViewBag.Clicks = db.Clicks
                 .Where(c => c.BookmarkId == id)
@@ -74,7 +87,7 @@ namespace Sonnetly.Controllers
         {
             var sonnet = db.Bookmarks.Where(b => b.NewUrl == newUrl).FirstOrDefault();
             string userName = User.Identity.GetUserName();
-            if (userName.Length == 0)
+            if (userName == null)
             {
                 userName = "Guest";
             }
@@ -94,5 +107,80 @@ namespace Sonnetly.Controllers
 
             return new RedirectResult(sonnet.OriginalUrl);
         }
+
+        // EDIT: Sonnet
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var userId = User.Identity.GetUserId();
+
+            Bookmarks bookmark = db.Bookmarks
+                .Where(b => b.OwnerId == userId)
+                .Where(b => b.Id == id)
+                .FirstOrDefault();
+
+            if (bookmark == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(bookmark);
+        }
+
+        // POST: Edit
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,Desc,OriginalUrl,NewUrl,NumClicks,OwnerId,Created")] Bookmarks sonnet)
+        {
+            var userId = User.Identity.GetUserId();
+            var userName = User.Identity.GetUserName();
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(sonnet).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sonnet);
+        }
+
+        // DELETE: Sonnet
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Bookmarks sonnet = db.Bookmarks.Find(id);
+
+            if (sonnet == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(sonnet);
+        }
+
+        // POST: Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Bookmarks sonnet = db.Bookmarks.Find(id);
+            db.Bookmarks.Remove(sonnet);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
